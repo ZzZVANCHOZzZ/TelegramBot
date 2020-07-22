27e299099a0b596992d662f6ac703274ab5d5b15
ZzZVANCHOZzZ <gamsss525@gmail.com>
2020-07-22 18:49:18 +0300

    Попытка добавить кнопку

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,5 +1,12 @@
 import pyowm
 import telebot
+from telebot import types
+
+start_markup = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
+start_markup_btn1 = types.KeyboardButton('/start')
+start_markup.add(start_markup_btn1)
+
+
 
 bot = telebot.TeleBot(
     '1327360961:AAGea4wpJuifaqERTKUtmvwwTpEyilYAizw')
